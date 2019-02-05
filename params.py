10e89e0b113a8f4e6208cb79a3fc1c8949b4ee39
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-02-05 17:31:05 +0100

    Update params.py

--- params.py
+++ params.py
@@ -2,17 +2,38 @@
 
 import sys
 import requests
-import optparse
 
-ModelPython = sys.version_info >= (3, 0)
-# Grrrrrrrrrrrrrrrrrr
-if(ModelPython):
-  sys.exit('{!} Incorrect version of python.')
+from optparse import *
+from modules import options
 
 # This project allows to test latex vulnerability to have good thing.
 # And to make a reverse shell for example.
 
 class LatexIncorrectSerial(Exception):
-  def __init__(self, LatexSerial=None):
-    # If an error has occurred in prog.
-    self.LatexSerial = LatexSerial
+	def __init__(self, LatexSerial=None):
+		'''
+		I create a specific function
+		for an exception for a reason.
+		'''
+		self.LatexSerial = LatexSerial
+
+class LatexArgumentation:
+	def __init__(self):
+		'''
+			Creating arguments to
+			do something cool.
+		'''
+		self.URL = options.ArgumentURL
+		self.POST = options.ArgumentPOST
+
+	def StringLT(self):
+		'''
+			The attack takes place
+			at this precise function.
+		'''
+		if(self.URL.startswith(("http://", "https://")) == False):
+			raise LatexIncorrectSerial("URL crashed !")
+
+if __name__ == "__main__":
+	Argument = LatexArgumentation()
+	Argument.StringLT()
