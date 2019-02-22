ec1a34f9cf2590777331110d8fe43c4e34023161
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-02-22 19:43:18 +0100

    Update look.py

--- look.py
+++ look.py
@@ -67,7 +67,7 @@ def PollersLangs():
 def PollersChatBox():
 	'''
 		This function allows to
-		see the status of the user.
+		see the chatbox of the user.
 	'''
 	ModelReqs = requests.get('https://www.root-me.org/'+USER).text
 	ModelServ = BeautifulSoup.BeautifulSoup(ModelReqs, "html5lib")
@@ -78,3 +78,24 @@ def PollersChatBox():
 
 	# He will leave the program with this command.
 	sys.exit("[+] ChatBox of the user : %s" %(ServiceLetter))
+
+@PollersUsers
+def PollersStatus():
+	'''
+		This function allows to
+		see the Status of the user.	
+	'''
+	ModelReqs = requests.get('https://www.root-me.org/'+USER).text
+	ModelServ = BeautifulSoup.BeautifulSoup(ModelReqs, "html5lib")
+
+	RegexStatus = re.findall('(<li>Statut|Status|estatus|&nbsp;:&nbsp;\w+.{0,}<\/li>)', ModelReqs)
+	if(type(RegexStatus) == list and len(RegexStatus) == 3):
+		RegexStatusService = RegexStatus[1]
+		RegexStatusService = RegexStatusService.replace("&nbsp;:&nbsp;", "")
+		RegexStatusService = RegexStatusService.replace("</li>", "")
+
+	# He will leave the program with this command.
+	sys.exit("[+] Status of the user : %s" %(RegexStatusService))
+
+if __name__ == "__main__":
+	PollersStatus()
