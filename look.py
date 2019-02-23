7fc73fdd2fd345244ac3fc82c21513f9b73e4819
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-02-23 21:59:57 +0100

    Update look.py

--- look.py
+++ look.py
@@ -117,7 +117,7 @@ def PollersAppScript():
 	'''
 	ModelReqs = requests.get('https://www.root-me.org/%s?inc=score&lang=fr' %(USER)).text
 	RegexApp = re.findall('(\/App-Script\/[A-Z0-9\-[a-z]+"\stitle="[0-9]{0,2}\s[A-Za-z]+">\s[ox])', ModelReqs)
-	# This code will allow you to see the machines hack into the AppScript.
+	
 	for ServiceParameter in RegexApp:
 		ServiceParameter = ServiceParameter.split("/")
 		ServiceParameter = ServiceParameter[2].split('"')
@@ -133,3 +133,6 @@ def PollersAppScript():
 		elif("x" in OwnsChallenge):
 			print(color.R+"[-] %s : Not Owned" %(NameChallenge))
 
+	
+
+			
